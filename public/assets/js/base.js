2257900d76240d7bac44241ee53bb02c88e984f1
cooptechnologyrd <cooptechnologyrd@gmail.com>
2019-05-31 12:35:17 -0400

    barra pricipal parte 1

---
+++ public/assets/js/base.js
@@ -0,0 +1,14 @@
+function showSearchBar() {
+    var topSearchBar = document.getElementById("top-search-bar");
+    topSearchBar.style.display='block';
+}
+
+window.addEventListener('mouseup',function(event){
+    var topSearchBar = document.getElementById('top-search-bar');
+    var inputBuscadorGlobal = document.getElementById('inputBuscadorGlobal');
+    var lupaInputBuscadorGlobal = document.getElementById('lupaInputBuscadorGlobal');
+    var divLupaInputBuscadorGlobal = document.getElementById('divLupaInputBuscadorGlobal');
+    if(event.target != divLupaInputBuscadorGlobal && event.target != lupaInputBuscadorGlobal && event.target != inputBuscadorGlobal && event.target != topSearchBar && event.target.parentNode != topSearchBar && event.target.parentNode.parentNode != topSearchBar){
+        topSearchBar.style.display = 'none';
+    }
+});
